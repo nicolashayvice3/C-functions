824d7f01ea06bf644ceeb97771db2bffd61b9bce
nicolashayvice3 <nicolashayvice@gmail.com>
2022-08-22 12:12:54 +1200

    if statement fixed list

--- c++/list.cpp
+++ c++/list.cpp
@@ -9,42 +9,42 @@ int main() {
   cin >> b;
   cout << endl;
 
-  if (a < b) {
-    cout << "Numbers divisible by 7 from " << a << " to " << b << endl;
-    cout << "******************************" << endl;
-    for (i = a; i < b; i++){
-        if (i % 7 == 0){
-            cout << i << ", ";
-        }
-    }
-    cout << endl;
-    cout << endl;
-    cout << "Prime numbers" << endl;
-    cout << "******************************" << endl;
-    cout<< "Prime List between" << "(" << a << " and " << b << ") :";
-    for (i = a; i < b; i++){
-        for (j = 2; j < i; j++){
-           if (i % j == 0){
-                l++;
-                break;
-            }
-        }
-        if(l == 0 && i != 1 && i != 2) {
-          cout << i << " ";
-          m++;
-        }
-        l = 0;
-    }
-    cout << endl; 
-    cout<< "Total number of prime numbers from" << "(" << a << " and " << b << ") :" << m << endl;
-    return 0;
-  } 
-  else {
-    cout << "Choose a starting value lower than an ending value, try again" << endl;
-    cout << "Enter starting number: ";
-    cin >> a;
-    cout << "Enter ending number: ";
-    cin >> b;
-    return 0;
-  } 
+  if (a > b) {
+  cout << "Choose a starting value lower than an ending value, try again" << endl;
+  cout << "Enter starting number: ";
+  cin >> a;
+  cout << "Enter ending number: ";
+  cin >> b;
+  }
+
+  cout << "Numbers divisible by 7 from " << a << " to " << b << endl;
+  cout << "******************************" << endl;
+  for (i = a; i < b; i++){
+      if (i % 7 == 0){
+          cout << i << ", ";
+      }
+  }
+
+  cout << endl;
+  cout << endl;
+  cout << "Prime numbers" << endl;
+  cout << "******************************" << endl;
+  cout<< "Prime List between" << "(" << a << " and " << b << ") :";
+  for (i = a; i < b; i++){
+      for (j = 2; j < i; j++){
+          if (i % j == 0){
+              l++;
+              break;
+          }
+      }
+      if(l == 0 && i != 1 && i != 2) {
+        cout << i << " ";
+        m++;
+      }
+      l = 0;
+  }
+  
+  cout << endl; 
+  cout<< "Total number of prime numbers from" << "(" << a << " and " << b << ") :" << m << endl;
+  return 0;
 }
