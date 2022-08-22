ec1aedae32a3fdc65b324692f07632e0ea4ab74f
nicolashayvice3 <nicolashayvice@gmail.com>
2022-08-23 11:56:51 +1200

    for loop prints no random

--- c++/random.cpp
+++ c++/random.cpp
@@ -4,16 +4,21 @@ using namespace std;
 
 
 int main() {
-  float a = 10, b = 20;
+  float a = 11, b = 20, i;
   string readyMessage = "";
   cout << "Random numbers between 10 - 20 with odd & even count" << endl;
   cout << "are you ready? ";
   cin >> readyMessage;
   cout << endl;
-  
+
+  cout << "Random numbers between 10 to 20" << endl;
+  cout << "******************************" << endl;
+
+  for (i = a; i < b; i++){
+    cout << i << ", ";
+  }
 
   if (a < 2) {
-
     return 0;
   } 
 }
