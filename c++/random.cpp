fbbd3405f23adbe90b74b3f270f3a8a41f086e6f
nicolashayvice3 <nicolashayvice@gmail.com>
2022-08-22 20:41:35 +1200

    random function start

---
+++ c++/random.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+
+int main() {
+  float a = 0;
+  cout << "Random numbers between 10 - 20 with odd & even count" << endl;
+
+  if (a < 2) {
+
+    return 0;
+  } 
+}
