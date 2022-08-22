0370c4d0129625923cc00f76ac4c049ab376464a
nicolashayvice3 <nicolashayvice@gmail.com>
2022-08-23 11:51:34 +1200

    welcome message random

--- c++/random.cpp
+++ c++/random.cpp
@@ -4,8 +4,13 @@ using namespace std;
 
 
 int main() {
-  float a = 0;
+  float a = 10, b = 20;
+  string readyMessage = "";
   cout << "Random numbers between 10 - 20 with odd & even count" << endl;
+  cout << "are you ready? ";
+  cin >> readyMessage;
+  cout << endl;
+  
 
   if (a < 2) {
 
