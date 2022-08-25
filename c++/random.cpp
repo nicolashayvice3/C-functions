aaccdddc902cc420c17e0bc4c664fbe92f7c535f
nicolashayvice3 <nicolashayvice@gmail.com>
2022-08-25 14:08:11 +1200

    comments added to random

--- c++/random.cpp
+++ c++/random.cpp
@@ -1,16 +1,21 @@
 #include <iostream>
+// for general purpose functions such as srand - initalize random number generator
 #include <cstdlib>
+// gets the time
 #include <ctime>
 using namespace std;
 
 
 int main() {
+
+  // variables
   int minimum = 11, rangefromMin = 9;
   int i;
   int totalTimes = 6;
   int a = 0, b = 0;
   bool first = true;
   string readyMessage = "";
+  // generate random number with time
   srand(time(0));
 
   cout << "Random numbers between 10 - 20 with odd & even count" << endl;
@@ -21,9 +26,12 @@ int main() {
   cout << "Random numbers between 10 to 20" << endl;
   cout << "******************************" << endl;
 
+  // for loop to stack all random numbers with a "," in between
+  // Also adds +1 to a and be based on the number being odd or even
   for (i = 1; i <= totalTimes; i++){
     int randomNumber = rand() % rangefromMin + minimum;
 
+    // make sure that it does not end with a ","
     if( first ) first = false;
     else cout << ", ";
     cout << randomNumber;
